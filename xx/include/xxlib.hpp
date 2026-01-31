5ad0a7b59fb94b3f97048b86fc157857c0af4d62
Gallardo994 <gx994@proton.me>
2026-01-31 19:08:15 +0400

    Bump to 0.3.0

--- xx/include/xxlib.hpp
+++ xx/include/xxlib.hpp
@@ -1,7 +1,7 @@
 #ifndef XXLIB_HPP
 #define XXLIB_HPP
 
-constexpr const char* XXLIB_VERSION = "0.2.1";
+constexpr const char* XXLIB_VERSION = "0.3.0";
 
 #include "detail/parser.hpp"
 #include "detail/planner.hpp"
