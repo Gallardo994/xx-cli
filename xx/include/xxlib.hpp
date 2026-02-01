e98e41b0dda7fc541def080321783fc12b7f1f9b
Gallardo994 <gx994@proton.me>
2026-02-02 03:18:12 +0400

    Fix forgotten rename

--- xx/include/xxlib.hpp
+++ xx/include/xxlib.hpp
@@ -5,7 +5,7 @@ constexpr const char* XXLIB_VERSION = "0.5.1";
 
 #include "detail/parser.hpp"
 #include "detail/planner.hpp"
-#include "detail/executor.hpp"
+#include "detail/platform_executor.hpp"
 #include "detail/luavm.hpp"
 
 #endif // XXLIB_HPP
